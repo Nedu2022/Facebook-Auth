ee0c7b6fef5c41254882567df4fdf6d76063022a
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-02-26 23:45:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Facebook from './components/Facebook';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="App-title">Facebook Auth Example</h1>
+        </header>
+        <p className='App-intro'>
+         To get started, authenticate with Facebook.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Facebook />
     </div>
   );
 }
---
+++ src/components/Facebook.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login'
+
+export default class Facebook extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: '',
+    name: '',
+    email: '',
+    picture: ''
+  }
+
+  responseFacebook = response => {
+    console.log(response);
+  }
+  componentClicked = () => console.log('clicked');
+  render() {
+    let fbContent;
+
+    if(this.state.isLoggedIn){
+      fbContent = null;
+    } else {
+      fbContent = ( <FacebookLogin
+        appId="586408003389545"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={this.componentClicked}
+        callback={this.responseFacebook} />);
+    }
+
+    return (
+      <div>${fbContent}</div>
+    )
+  }
+}
+
